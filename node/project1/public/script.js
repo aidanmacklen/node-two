a391391aaaf843b8cc242ad809a78f6df9597503
Aidan Macklen <aidancmacklen@gmail.com>
2023-11-07 01:04:08 -0500

    push

---
+++ node/project1/public/script.js
@@ -0,0 +1,58 @@
+const showWarriors = async() => {
+    const warriorsJSON = await getWarriors();
+    const warriorsDiv = document.getElementById("warriors-div");
+
+    if (warriorsJSON == "") {
+        console.log("Invalid json");
+        return;
+    }
+
+    warriorsJSON.forEach((warrior) => {
+        const section = document.createElement("section");
+        warriorsDiv.append(section);
+
+        const Nameh2 = document.createElement("h2");
+        Nameh2.innerHTML = warrior.name;
+        section.append(Nameh2);
+
+        const Weaponp = document.createElement("p");
+        Weaponp.innerHTML = `<strong>Weapon</strong>: ${warrior.weapon}`;
+        section.append(Weaponp);
+
+        const Attackp = document.createElement("p");
+        Attackp.innerHTML = `<strong>Attack</strong>: ${warrior.attack}`;
+        section.append(Attackp);
+
+        const Behaviorp = document.createElement("p");
+        Behaviorp.innerHTML = `<strong>Behavior</strong>: ${warrior.behavior}`;
+        section.append(Behaviorp);
+
+        const Featuresul = document.createElement("ul");
+        section.append(Featuresul);
+    
+        for (let i in warrior.features) {
+            const Featuresli = document.createElement("li");
+            Featuresul.append(Featuresli);
+            Featuresli.innerHTML = warrior.features[i];
+        }
+
+        const img = document.createElement("img");
+        img.src = "http://localhost:3000/" + warrior.img;
+        section.append(img);
+
+        const hr = document.createElement("hr");
+        section.append(hr);
+    });
+
+}
+
+const getWarriors = async() => {
+    try {
+        return (await fetch("http://localhost:3000/api/warriors")).json();
+    } catch (error) {
+        console.log("error getting json");
+        return "";
+    }
+}
+
+window.onload = () => showWarriors();
